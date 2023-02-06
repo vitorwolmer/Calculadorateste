a14a966d64129b70731858252c8bfc4847ffcdf8
Vitor <wolmervitor@gmail.com>
2023-02-06 20:03:12 -0300

    implementando cálculo de idade por matrizes

--- calculadorateste/calculadorateste/Program.cs
+++ calculadorateste/calculadorateste/Program.cs
@@ -1,87 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var planilha = new string[4,2];
 
-Console.WriteLine("Maconha!!!!:");
+planilha[0,0] = "Josefina";
+planilha[0, 1] = "65";
 
+planilha[1, 0] = "Seu Dário";
+planilha[1, 1] = "75";
 
-double N1;
-double N2;
-double A;
-double S;
-double M;
-double D;
-char escolha;
+planilha[2, 0] = "Kátia";
+planilha[2, 1] = "12";
 
+planilha[3, 0] = "Amauri";
+planilha[3, 1] = "25";
 
-Console.WriteLine("Qual operação deseja realizar: [a]Adição  [s]Subtração  [m]Multiplicação  [d]Divisão");
-escolha= Char.Parse(Console.ReadLine());
-
-
-        switch (escolha)
-        {
-            case 'A':
-            case 'a':
-        
-                Console.WriteLine("Você escolheu Adição:");
-                Console.WriteLine("Digite o primeiro número:");
-                N1 = Double.Parse(Console.ReadLine());
-                Console.WriteLine("Digite o segundo número:");
-                N2 = Double.Parse(Console.ReadLine());
-
-                A = N1 + N2;
-                Console.WriteLine($"O Resultado da adição é: {A}");
-                break;
+for (int i = 0; i < 4; i++)
+{
+	for (int j = 0; j < 2; j++)
+	{
+		if (j == 1)
+		{
+           var idade = Convert.ToInt32( planilha[i, j] );
+			if (idade < 5)
+			{
+				planilha[i, j] = "não paga";
 
 
-            case 'S':
-            case 's':
-      
-                Console.WriteLine("Você escolheu Subtração:");
-                Console.WriteLine("Digite o primeiro número:");
-                N1 = Double.Parse(Console.ReadLine());
-                Console.WriteLine("Digite o segundo número:");
-                N2 = Double.Parse(Console.ReadLine());
+			}
+			else if (idade >= 5 && idade < 65)
+			{
+				planilha[i, j] = "Paga inteira";
 
-                S = N1 - N2;
-                Console.WriteLine($"O Resultado da Subtração é: {S}");
-                break;
+			}
+			else 
+			{
+                planilha[i, j] = "Paga meia";
+            }
 
-
-            case 'M':
-            case 'm':
-       
-                Console.WriteLine("Você escolheu Multiplicação:");
-                Console.WriteLine("Digite o primeiro número:");
-                N1 = Double.Parse(Console.ReadLine());
-                Console.WriteLine("Digite o segundo número:");
-                N2 = Double.Parse(Console.ReadLine());
-
-                M = N1 * N2;
-                Console.WriteLine($"O Resultado da multiplicação é: {M}");
-
-                break;
-
-
-            case 'D':
-            case 'd':
-        
-                Console.WriteLine("Você escolheu Divisão:");
-                Console.WriteLine("Digite o primeiro número:");
-                N1 = Double.Parse(Console.ReadLine());
-                Console.WriteLine("Digite o segundo número:");
-                N2 = Double.Parse(Console.ReadLine());
-
-                D = N1 / N2;
-                Console.WriteLine($"O Resultado da divisão é: {D}");
-
-                break;
-
-            default:
-        
-                Console.WriteLine("Opção inválida. Escolha novamente.Aperte qualquer tecla para voltar.");
-                Console.ReadKey();
-                break;
         }
+    }
+}
 
 
+Console.WriteLine("SISTEMA HOSPEDAGEM");
 
-
+Console.WriteLine("por favor digite seu nome : ");
+Console.ReadLine();
